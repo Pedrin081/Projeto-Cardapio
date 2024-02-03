d8bab1878776f71647bceb985a8ac3e33f926fec
Pedro Victor <pedro.vnascimento@outlook.com>
2024-02-02 22:47:05 -0300

    inicio projeto em react

---
+++ Hashtaurante/src/App.jsx
@@ -0,0 +1,13 @@
+import hashtaurante from "./assets/hashtaurante.webp"; 
+import './App.css'
+import { Navegacao } from "./Navegacao"
+
+
+export function App() {
+
+  
+  return <>
+    <img src={hashtaurante} alt="" className="capa"/>    
+    <Navegacao/>
+     </>
+    }
---
+++ Hashtaurante/src/Navegacao.jsx
@@ -0,0 +1,9 @@
+export function Navegacao() {  
+
+    return <div>    
+        <input type="radio" name="opcao-pagina" id="pagina-0" defaultChecked />
+        <input type="radio" name="opcao-pagina" id="pagina-1" />
+        <input type="radio" name="opcao-pagina" id="pagina-2" />
+    </div>
+
+}
